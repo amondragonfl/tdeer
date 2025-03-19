2c41458d346e10be049d7c73a7739ea4adcbdbab
Alejandro <amondragonflrs@gmail.com>
2025-03-19 17:59:28 -0500

    Minimum viable working estimator

--- tdeer/main.py
+++ tdeer/main.py
@@ -1,11 +1,17 @@
 import argparse
 from tdeer.utils.csv_processing import validate_csv, read_csv_as_numpy
-from tdeer.utils.statistical_analysis import predict_tdee
+from tdeer.utils.statistical_analysis import predict_tdee_with_linear_regression
 
 def main ():
+    print("[TDEER] Accurately estimate your daily calories")
     parser = argparse.ArgumentParser(description="Accurately estimate your TDEE.")
     parser.add_argument("file_path", type=str, help="Path to CSV file containing weight and calorie data")
     args = parser.parse_args()
+
+    if validate_csv(args.file_path):
+        weights, calories =  read_csv_as_numpy(args.file_path)
+        tdee_est_linear_regression = round(predict_tdee_with_linear_regression(weights, calories))
+        print(f"[*] linear regression estimation: {str(tdee_est_linear_regression)} calories")
 
 if __name__ == "__main__":
     main()
