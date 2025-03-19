59505b3e47aa19b1f8b4c36d66e852a188481398
Alejandro <amondragonflrs@gmail.com>
2025-03-19 18:52:17 -0500

    Removed unnecessary requirement

--- tdeer/utils/statistical_analysis.py
+++ tdeer/utils/statistical_analysis.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.covariance import EllipticEnvelope
