4a03532c6867c00556cf827f33a14ab935be8530
Alejandro <amondragonflrs@gmail.com>
2025-03-19 18:51:06 -0500

    Minor aesthethic adjustment

--- tdeer/main.py
+++ tdeer/main.py
@@ -3,7 +3,6 @@ from tdeer.utils.csv_processing import validate_csv, read_csv_as_numpy
 from tdeer.utils.statistical_analysis import predict_tdee_with_linear_regression
 
 def main ():
-    print("[TDEER] Accurately estimate your daily calories")
     parser = argparse.ArgumentParser(description="Accurately estimate your TDEE.")
     parser.add_argument("file_path", type=str, help="Path to CSV file containing weight and calorie data")
     args = parser.parse_args()
@@ -11,7 +10,7 @@ def main ():
     if validate_csv(args.file_path):
         weights, calories =  read_csv_as_numpy(args.file_path)
         tdee_est_linear_regression = round(predict_tdee_with_linear_regression(weights, calories))
-        print(f"[*] linear regression estimation: {str(tdee_est_linear_regression)} calories")
+        print(f"[TDEER] estimates your tdee to be {str(tdee_est_linear_regression)} calories")
 
 if __name__ == "__main__":
     main()
